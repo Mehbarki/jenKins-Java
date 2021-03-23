a7f484a7fc1b23e8535ec8959ffc034845d71ec6
Mehdi MEBARKI <m.mebarki@ecole-ipssi.net>
2021-03-23 12:52:25 +0100

    add java file

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nouvelle Compilation auto via WebHook Githubb avec validation jenkins URL!");
+    }
+}
+
